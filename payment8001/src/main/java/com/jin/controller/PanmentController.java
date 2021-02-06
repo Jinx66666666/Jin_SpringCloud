4cba29dfa4858af65da3bf8bd9a33dd04e05ca81
Jin <13273023452@163.com>
2021-02-06 14:35:21 +0800

    actuator微服务信息完善,服务发现Discovery,Eureka自我保护

--- payment8001/src/main/java/com/jin/controller/PanmentController.java
+++ payment8001/src/main/java/com/jin/controller/PanmentController.java
@@ -5,9 +5,12 @@ import com.jin.entity.Payment;
 import com.jin.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author: Jin
@@ -23,6 +26,8 @@ public class PanmentController {
     private PaymentService paymentService;
     @Value("${server.port}")
     private String serverport;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/save")
     public CommonResult save(@RequestBody Payment payment) {
@@ -47,5 +52,18 @@ public class PanmentController {
         } else {
             return new CommonResult(500, "查询数据失败,参数id为" + id + ",服务器为:" + serverport, null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("===服务信息:" + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info("===地址信息:" + instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
