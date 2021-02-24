1c22cbbc30a7356d01c327fa5974d6d432c5139a
Jin <13273023452@163.com>
2021-02-24 18:06:05 +0800

    Config服务端配置

---
+++ config-center3344/src/main/java/com/jin/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: Jin
+ * @Date: 2021/2/24 16:02
+ * @Description: ConfigCenterMain3344
+ * @version: V1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
