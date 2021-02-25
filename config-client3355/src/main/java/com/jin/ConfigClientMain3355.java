a9e0da7cdac41494e47b400d4e59bcb45fec5859
Jin <13273023452@163.com>
2021-02-25 10:00:12 +0800

    Config客户端配置

---
+++ config-client3355/src/main/java/com/jin/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: Jin
+ * @Date: 2021/2/25 9:31
+ * @Description:
+ * @version: V1.0
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
