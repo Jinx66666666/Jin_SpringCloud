819c9c3082e30ab1fb37eab7199a69b99f6eb876
Jin <13273023452@163.com>
2021-03-02 18:06:10 +0800

    Sentinel按资源名称限流+后续处理

---
+++ sentinel-service-8401/src/main/java/com/jin/controller/RateLimitController.java
@@ -0,0 +1,27 @@
+package com.jin.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.jin.entity.CommonResult;
+import com.jin.entity.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Jin
+ * @Date: 2021/3/2 17:11
+ * @Description: RateLimitController
+ * @version: V1.0
+ */
+@RestController
+public class RateLimitController {
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource", blockHandler = "handleException")
+    public CommonResult byResource() {
+        return new CommonResult(200, "按资源名称限流测试OK", new Payment(2020L, "serial001"));
+    }
+
+    public CommonResult handleException(BlockException exception) {
+        return new CommonResult(444, exception.getClass().getCanonicalName() + "\t 服务不可用");
+    }
+}
